737724c84076d2ab9db9e8fa068f6255ed6bd1ef
Cassiano-code <71021277+Cassiano-code@users.noreply.github.com>
2020-10-22 12:46:11 -0300

    Add files via upload

---
+++ salvar.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome = $_POST["txtNome"];
+$altura = $_POST["txtAltura"];
+$peso = $_POST["txtPeso"];
+echo "Nome: ".$nome."\n"; 
+echo "Altura: ".$altura."\n"; 
+echo "peso: ".$peso."\n"; 
